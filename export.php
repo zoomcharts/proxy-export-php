176b4e8eaf375b8b7855b9f635aa2a9002ca1509
Eižens Slava <eizens@zoomcharts.com>
2016-04-28 14:06:15 +0300

    php proxy full rewrite. Added test.html

--- export.php
+++ export.php
@@ -2,69 +2,67 @@
 //call this function:
 export();
 function export(){
-    $type = $_POST["type"];
-    $valid = array("image/jpeg"=>"jpeg", "image/png"=>"png", "application/pdf"=>"pdf", "image/svg" => "svg", "text/csv" => "csv", "application/vnd.ms-excel" => "xls");
-    if (isset($valid[$type])){
-        $ext = $valid[$type];
-        if ($_POST["encoding"] == "base64"){
-            $c = $_POST["data"];
-            if ($p = strpos($_POST["data"], ",")){
-                $c = substr($_POST["data"], $p + 1);
-            }
-            $c = base64_decode($c);
-        } else if ($_POST["encoding"] == "json") {
-            $c = json_decode($_POST["data"]);
-        } else {
-            $c = $_POST["data"];
-        }
-        if (!$c){
-            echo "Sorry, could not export this image";
-            exit;
-        }
-        header("Content-Type: " . $type);
-        header("Content-Disposition: attachment; filename=\"export.$ext\"");
-        if ($ext == "xls"){
-            /* convert json to xls */
-            print("<table>");
-            foreach ($c as $row){
-                print("<tr><td>".implode("</td><td>", $row)."</td></tr>");
-            }
-            print("</table>");
-            exit;
-        } else if ($ext == "csv"){
-            /* convert to csv */
-            $locale = "lv_LV";
-            $fmt = numfmt_create($locale, NumberFormatter::PATTERN_DECIMAL);
-            foreach ($c as $row){
-                array_walk($row, function(&$item,$key) use ($fmt){
-                    if ($key > 1){
-                        $item = $fmt->format($item);
-                    }
-
-                });
-                print(implode(";", $row)."\n");
-            }
-            exit;
-        } else if ($_POST["setdpi"]){
-            $cache_dir = "cache/tmp";
-            $fid = fopen($a = $cache_dir.microtime(true) . ".$ext", "w");
-            fputs($fid, $c);
-            fclose($fid);
-            $dpi = (int)$_POST["setdpi"];
-            if ($dpi < 36){
-                $dpi = 36;
-            } else if ($dpi > 1200){
-                $dpi = 1200;
-            }
-            system("LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 exiftool -Xresolution=$dpi -Yresolution=$dpi -overwrite_original -q $a");
-            print file_get_contents($a);
-            unlink($a);
-            exit;
-        }
-        print $c;
+    $p = $_POST;
+    if(!isset($p["data"]) || !$p["data"] || (substr($p["data"], 0, 5) != "data:")) {
+        echo "The request did not include a valid 'data' parameter which must be a valid data-uri.\n";
+        echo "Received input:\n";
+        echo $p["data"] ?: "null";
         exit;
+    }
+    $data = $p["data"];
+    $i = strpos($data, ",");
+    $j = strpos($data, ";");
+    if($j == false) $j = $i;
+    if($i == false) $i = $j;
+    
+    $type = substr($data, 5, $j - 5);
+    $valid = array("image/jpeg"=>"jpeg", "image/png"=>"png", "application/pdf"=>"pdf", "image/svg" => "svg", "text/csv" => "csv", "application/vnd.ms-excel" => "xls", "text/plain" => "txt");
+    if (!isset($type) || !$type || !isset($valid[$type])){
+        echo "Unsupported type.";
+        exit;   
     } else {
-        echo "Invalid request";
+        $ext = $valid[$type];
+    }
+
+    $mime = $type ?: "application/octet-stream";
+    $name = (isset($p["name"]) && $p["name"]) ? $p["name"] : "export";
+    $base64 = substr($data, $i - 7, 7) == ";base64";
+
+    //Proper name handling:   
+    $tmp_ext = ".".$ext;
+    if(strlen($tmp_ext) < strlen($name)) {
+        $ex = substr_compare($name, $tmp_ext, strlen($name) - strlen($tmp_ext), strlen($tmp_ext)) === 0;
+        if(!$ex) {
+            $name = $name.".".$ext;
+        }
+    }
+
+
+    try {
+        if($base64) {
+            if(!strpos($data, ",")) {
+                echo "Invalid data-uri format";
+            }
+            header("Content-Type: " . $mime);
+            header("Content-Disposition: attachment; filename=\"$name\"");
+            header("Cache-Control: private, must-revalidate, max-age=0");
+
+            $sub = substr($data, $i + 1);
+            $bdata = base64_decode($sub);
+            echo $bdata;
+            exit;
+        } else {
+            header("Content-Type: " . $mime);
+            header("Content-Disposition: attachment; filename=\"$name\"");
+            header("Cache-Control: private, must-revalidate, max-age=0");
+
+            $sub = substr($data, $i + 1);
+            echo urldecode($sub);
+        }
+    } catch(Exception $e) {
+        echo "The data-uri could not be parsed.\n";
+        echo $e->getMessage();
+        exit;
     }
     exit;
 }
